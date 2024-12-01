4016c59d7ca39850fa7dda6d25fd0b7fd970f42c
Artorias <105161738+artorias305@users.noreply.github.com>
2024-12-01 15:26:01 -0300

    Eza for ll

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -20,12 +20,15 @@ $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
 # Alias
 Set-Alias -Name vim -Value nvim
-Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias lg lazygit
+function ll {
+    eza -l -g --icons
+}
+
 
 # Utilities
 function which ($command) {
