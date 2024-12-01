356794b30662ad4763422f80d35898ad588e9d36
Artorias <105161738+artorias305@users.noreply.github.com>
2024-12-01 10:30:17 -0300

    Added README.md & image

---
+++ README.md
@@ -0,0 +1,9 @@
+# Artorias's Dotfiles
+
+![showcase](./images/showcase.png)
+
+## Contents
+
+- vim (neovim) config
+- PowerShellConfig
+- WezTerm config
--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -25,6 +25,7 @@ Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias lg lazygit
 
 # Utilities
 function which ($command) {
