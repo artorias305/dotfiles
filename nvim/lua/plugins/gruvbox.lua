f420fcd5ee222aa7715e326079bd2e31387902d7
Artorias <105161738+artorias305@users.noreply.github.com>
2024-12-05 22:48:15 -0300

    MORE GRUVBOX

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -3,4 +3,10 @@ return {
     "ellisonleao/gruvbox.nvim",
     config = true,
   },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
 }
--- nvim/lua/plugins/sonokai.lua
+++ nvim/lua/plugins/sonokai.lua
@@ -5,7 +5,6 @@ return {
       vim.g.sonokai_transparent_background = "1"
       vim.g.sonokai_enable_italic = "1"
       vim.g.sonokai_style = "andromeda"
-      vim.cmd.colorscheme("sonokai")
     end,
   },
 }
