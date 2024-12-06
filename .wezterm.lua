79357cf1574bf72dddd11c09d634b277a1617a4a
Artorias <105161738+artorias305@users.noreply.github.com>
2024-12-05 22:52:14 -0300

    wtffff the wezterm things

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,12 +14,13 @@ config.default_cursor_style = "BlinkingBlock"
 config.animation_fps = 1
 config.cursor_blink_rate = 500
 config.term = "xterm-256color" -- Set the terminal type
+config.color_scheme = "Gruvbox Dark (Gogh)"
 
 -- config.font = wezterm.font("Iosevka Custom")
 -- config.font = wezterm.font("Monocraft Nerd Font")
 -- config.font = wezterm.font("FiraCode Nerd Font")
 -- config.font = wezterm.font("Hack Nerd Font Mono")
-config.font = wezterm.font("JetBrains Mono Regular")
+config.font = wezterm.font("JetBrainsMono NF")
 config.cell_width = 0.9
 -- config.font = wezterm.font("Menlo Regular")
 -- config.font = wezterm.font("Hasklig")
@@ -30,9 +31,9 @@ config.cell_width = 0.9
 -- config.font = wezterm.font("M+ 1m")
 -- config.font = wezterm.font("Hack Regular")
 -- config.cell_width = 0.9
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 config.prefer_egl = true
-config.font_size = 14.0
+-- config.font_size = 12.0
 
 config.window_padding = {
 	left = 0,
@@ -55,16 +56,16 @@ config.use_fancy_tab_bar = false
 --
 
 -- color scheme toggling
-wezterm.on("toggle-colorscheme", function(window, pane)
-	local overrides = window:get_config_overrides() or {}
-	if overrides.color_scheme == "Zenburn" then
-		overrides.color_scheme = "Cloud (terminal.sexy)"
-	else
-		overrides.color_scheme = "Zenburn"
-	end
-	window:set_config_overrides(overrides)
-end)
-
+-- wezterm.on("toggle-colorscheme", function(window, pane)
+-- 	local overrides = window:get_config_overrides() or {}
+-- 	if overrides.color_scheme == "Zenburn" then
+-- 		overrides.color_scheme = "Cloud (terminal.sexy)"
+-- 	else
+-- 		overrides.color_scheme = "Zenburn"
+-- 	end
+-- 	window:set_config_overrides(overrides)
+-- end)
+--
 -- keymaps
 config.keys = {
 	{
@@ -127,75 +128,75 @@ config.keys = {
 }
 
 -- For example, changing the color scheme:
-config.color_scheme = "Cloud (terminal.sexy)"
-config.colors = {
-	-- background = '#3b224c',
-	-- background = "#181616", -- vague.nvim bg
-	-- background = "#080808", -- almost black
-	background = "#0c0b0f", -- dark purple
-	-- background = "#020202", -- dark purple
-	-- background = "#17151c", -- brighter purple
-	-- background = "#16141a",
-	-- background = "#0e0e12", -- bright washed lavendar
-	-- background = 'rgba(59, 34, 76, 100%)',
-	cursor_border = "#bea3c7",
-	-- cursor_fg = "#281733",
-	cursor_bg = "#bea3c7",
-	-- selection_fg = '#281733',
-
-	tab_bar = {
-		background = "#0c0b0f",
-		-- background = "rgba(0, 0, 0, 0%)",
-		active_tab = {
-			bg_color = "#0c0b0f",
-			fg_color = "#bea3c7",
-			intensity = "Normal",
-			underline = "None",
-			italic = false,
-			strikethrough = false,
-		},
-		inactive_tab = {
-			bg_color = "#0c0b0f",
-			fg_color = "#f8f2f5",
-			intensity = "Normal",
-			underline = "None",
-			italic = false,
-			strikethrough = false,
-		},
-
-		new_tab = {
-			-- bg_color = "rgba(59, 34, 76, 50%)",
-			bg_color = "#0c0b0f",
-			fg_color = "white",
-		},
-	},
-}
-
-config.window_frame = {
-	font = wezterm.font({ family = "Iosevka Custom", weight = "Regular" }),
-	active_titlebar_bg = "#0c0b0f",
-	-- active_titlebar_bg = "#181616",
-}
-
--- config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
+-- config.color_scheme = "Cloud (terminal.sexy)"
+-- config.colors = {
+-- 	-- background = '#3b224c',
+-- 	-- background = "#181616", -- vague.nvim bg
+-- 	-- background = "#080808", -- almost black
+-- 	background = "#0c0b0f", -- dark purple
+-- 	-- background = "#020202", -- dark purple
+-- 	-- background = "#17151c", -- brighter purple
+-- 	-- background = "#16141a",
+-- 	-- background = "#0e0e12", -- bright washed lavendar
+-- 	-- background = 'rgba(59, 34, 76, 100%)',
+-- 	cursor_border = "#bea3c7",
+-- 	-- cursor_fg = "#281733",
+-- 	cursor_bg = "#bea3c7",
+-- 	-- selection_fg = '#281733',
+--
+-- 	tab_bar = {
+-- 		background = "#0c0b0f",
+-- 		-- background = "rgba(0, 0, 0, 0%)",
+-- 		active_tab = {
+-- 			bg_color = "#0c0b0f",
+-- 			fg_color = "#bea3c7",
+-- 			intensity = "Normal",
+-- 			underline = "None",
+-- 			italic = false,
+-- 			strikethrough = false,
+-- 		},
+-- 		inactive_tab = {
+-- 			bg_color = "#0c0b0f",
+-- 			fg_color = "#f8f2f5",
+-- 			intensity = "Normal",
+-- 			underline = "None",
+-- 			italic = false,
+-- 			strikethrough = false,
+-- 		},
+--
+-- 		new_tab = {
+-- 			-- bg_color = "rgba(59, 34, 76, 50%)",
+-- 			bg_color = "#0c0b0f",
+-- 			fg_color = "white",
+-- 		},
+-- 	},
+-- }
+--
+-- config.window_frame = {
+-- 	font = wezterm.font({ family = "Iosevka Custom", weight = "Regular" }),
+-- 	active_titlebar_bg = "#0c0b0f",
+-- 	-- active_titlebar_bg = "#181616",
+-- }
+--
+-- -- config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.window_decorations = "NONE | RESIZE"
 config.default_prog = { "powershell.exe", "-NoLogo" }
-config.initial_cols = 80
--- config.window_background_image = "C:/dev/misc/berk.png"
--- config.window_background_image_hsb = {
--- 	brightness = 0.1,
--- }
-
--- wezterm.on("gui-startup", function(cmd)
--- 	local args = {}
--- 	if cmd then
--- 		args = cmd.args
--- 	end
+-- config.initial_cols = 80
+-- -- config.window_background_image = "C:/dev/misc/berk.png"
+-- -- config.window_background_image_hsb = {
+-- -- 	brightness = 0.1,
+-- -- }
 --
--- 	local tab, pane, window = mux.spawn_window(cmd or {})
--- 	-- window:gui_window():maximize()
--- 	-- window:gui_window():set_position(0, 0)
--- end)
-
+-- -- wezterm.on("gui-startup", function(cmd)
+-- -- 	local args = {}
+-- -- 	if cmd then
+-- -- 		args = cmd.args
+-- -- 	end
+-- --
+-- -- 	local tab, pane, window = mux.spawn_window(cmd or {})
+-- -- 	-- window:gui_window():maximize()
+-- -- 	-- window:gui_window():set_position(0, 0)
+-- -- end)
+--
 -- and finally, return the configuration to wezterm
 return config
