303b570a29659f113ba0d0fef996ad94e99f5d6f
Artorias <105161738+artorias305@users.noreply.github.com>
2024-12-05 22:33:05 -0300

    GRUVBOX

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -2,5 +2,23 @@ return {
   {
     "rose-pine/neovim",
     lazy = false,
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+        dark_variant = "moon",
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+        enable = {
+          terminal = true,
+          legacy_highlights = true,
+          migrations = true,
+        },
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = true,
+        },
+      })
+    end,
   },
 }
